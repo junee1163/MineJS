3fc67c5e262213c1511aba84ca26acf19ea49730
junee <junee1163@gmail.com>
2016-10-02 18:18:37 +0900

    List of blocks (not all)[forked from nukkit]
    Copied from Nukkit

---
+++ sources/block/block.js
@@ -0,0 +1,251 @@
+var AIR = 0;
+    var STONE = 1;
+    var GRASS = 2;
+    var DIRT = 3;
+    var COBBLESTONE = 4;
+    var COBBLE = 4;
+    var PLANK = 5;
+    var PLANKS = 5;
+    var WOODEN_PLANK = 5;
+    var WOODEN_PLANKS = 5;
+    var SAPLING = 6;
+    var SAPLINGS = 6;
+    var BEDROCK = 7;
+    var WATER = 8;
+    var STILL_WATER = 9;
+    var LAVA = 10;
+    var STILL_LAVA = 11;
+    var SAND = 12;
+    var GRAVEL = 13;
+    var GOLD_ORE = 14;
+    var IRON_ORE = 15;
+    var COAL_ORE = 16;
+    var LOG = 17;
+    var WOOD = 17;
+    var TRUNK = 17;
+    var LEAVES = 18;
+    var LEAVE = 18;
+    var SPONGE = 19;
+    var GLASS = 20;
+    var LAPIS_ORE = 21;
+    var LAPIS_BLOCK = 22;
+
+    var DISPENSER = 23;
+
+    var SANDSTONE = 24;
+    var NOTEBLOCK = 25;
+    var BED_BLOCK = 26;
+    var POWERED_RAIL = 27;
+    var DETECTOR_RAIL = 28;
+
+    var COBWEB = 30;
+    var TALL_GRASS = 31;
+    var BUSH = 32;
+    var DEAD_BUSH = 32;
+    var WOOL = 35;
+    var DANDELION = 37;
+    var POPPY = 38;
+    var ROSE = 38;
+    var FLOWER = 38;
+    var RED_FLOWER = 38;
+    var BROWN_MUSHROOM = 39;
+    var RED_MUSHROOM = 40;
+    var GOLD_BLOCK = 41;
+    var IRON_BLOCK = 42;
+    var DOUBLE_SLAB = 43;
+    var DOUBLE_SLABS = 43;
+    var SLAB = 44;
+    var SLABS = 44;
+    var BRICKS = 45;
+    var BRICKS_BLOCK = 45;
+    var TNT = 46;
+    var BOOKSHELF = 47;
+    var MOSS_STONE = 48;
+    var MOSSY_STONE = 48;
+    var OBSIDIAN = 49;
+    var TORCH = 50;
+    var FIRE = 51;
+    var MONSTER_SPAWNER = 52;
+    var WOOD_STAIRS = 53;
+    var WOODEN_STAIRS = 53;
+    var OAK_WOOD_STAIRS = 53;
+    var OAK_WOODEN_STAIRS = 53;
+    var CHEST = 54;
+    var REDSTONE_WIRE = 55;
+    var DIAMOND_ORE = 56;
+    var DIAMOND_BLOCK = 57;
+    var CRAFTING_TABLE = 58;
+    var WORKBENCH = 58;
+    var WHEAT_BLOCK = 59;
+    var FARMLAND = 60;
+    var FURNACE = 61;
+    var BURNING_FURNACE = 62;
+    var LIT_FURNACE = 62;
+    var SIGN_POST = 63;
+    var DOOR_BLOCK = 64;
+    var WOODEN_DOOR_BLOCK = 64;
+    var WOOD_DOOR_BLOCK = 64;
+    var LADDER = 65;
+    var RAIL = 66;
+    var COBBLE_STAIRS = 67;
+    var COBBLESTONE_STAIRS = 67;
+    var WALL_SIGN = 68;
+    var LEVER = 69;
+    var STONE_PRESSURE_PLATE = 70;
+    var IRON_DOOR_BLOCK = 71;
+    var WOODEN_PRESSURE_PLATE = 72;
+    var REDSTONE_ORE = 73;
+    var GLOWING_REDSTONE_ORE = 74;
+    var LIT_REDSTONE_ORE = 74;
+
+    var REDSTONE_TORCH = 76;
+
+    var SNOW = 78;
+    var SNOW_LAYER = 78;
+    var ICE = 79;
+    var SNOW_BLOCK = 80;
+    var CACTUS = 81;
+    var CLAY_BLOCK = 82;
+    var REEDS = 83;
+    var SUGARCANE_BLOCK = 83;
+
+    var FENCE = 85;
+    var PUMPKIN = 86;
+    var NETHERRACK = 87;
+    var SOUL_SAND = 88;
+    var GLOWSTONE = 89;
+    var GLOWSTONE_BLOCK = 89;
+    var NETHER_PORTAL = 90;
+    var LIT_PUMPKIN = 91;
+    var JACK_O_LANTERN = 91;
+    var CAKE_BLOCK = 92;
+
+    var INVISIBLE_BEDROCK = 95;
+    var TRAPDOOR = 96;
+
+    var STONE_BRICKS = 98;
+    var STONE_BRICK = 98;
+
+    var BROWN_MUSHROOM_BLOCK = 99;
+    var RED_MUSHROOM_BLOCK = 100;
+
+    var IRON_BAR = 101;
+    var IRON_BARS = 101;
+    var GLASS_PANE = 102;
+    var GLASS_PANEL = 102;
+    var MELON_BLOCK = 103;
+    var PUMPKIN_STEM = 104;
+    var MELON_STEM = 105;
+    var VINE = 106;
+    var VINES = 106;
+    var FENCE_GATE = 107;
+    var FENCE_GATE_OAK = 107;
+    var BRICK_STAIRS = 108;
+    var STONE_BRICK_STAIRS = 109;
+    var MYCELIUM = 110;
+    var WATER_LILY = 111;
+    var LILY_PAD = 111;
+    var NETHER_BRICKS = 112;
+    var NETHER_BRICK_BLOCK = 112;
+    var NETHER_BRICK_FENCE = 113;
+    var NETHER_BRICKS_STAIRS = 114;
+
+    var ENCHANTING_TABLE = 116;
+    var ENCHANT_TABLE = 116;
+    var ENCHANTMENT_TABLE = 116;
+    var BREWING_STAND_BLOCK = 117;
+    var BREWING_BLOCK = 117;
+    var CAULDRON_BLOCK = 118;
+    var END_PORTAL = 119;
+    var END_PORTAL_FRAME = 120;
+    var END_STONE = 121;
+
+    var REDSTONE_LAMP = 123;
+    var LIT_REDSTONE_LAMP = 124;
+
+    var ACTIVATOR_RAIL = 126;
+
+    var SANDSTONE_STAIRS = 128;
+    var EMERALD_ORE = 129;
+
+    var EMERALD_BLOCK = 133;
+    var SPRUCE_WOOD_STAIRS = 134;
+    var SPRUCE_WOODEN_STAIRS = 134;
+    var BIRCH_WOOD_STAIRS = 135;
+    var BIRCH_WOODEN_STAIRS = 135;
+    var JUNGLE_WOOD_STAIRS = 136;
+    var JUNGLE_WOODEN_STAIRS = 136;
+
+    var COBBLE_WALL = 139;
+    var STONE_WALL = 139;
+    var COBBLESTONE_WALL = 139;
+    var FLOWER_POT_BLOCK = 140;
+    var CARROT_BLOCK = 141;
+    var POTATO_BLOCK = 142;
+
+    var SKULL_BLOCK = 144;
+    var ANVIL = 145;
+    var TRAPPED_CHEST = 146;
+    var LIGHT_WEIGHTED_PRESSURE_PLATE = 147;
+    var HEAVY_WEIGHTED_PRESSURE_PLATE = 148;
+    var UNPOWERED_COMPARATOR = 149;
+    var POWERED_COMPARATOR = 150;
+    var DAYLIGHT_DETECTOR = 151;
+    var REDSTONE_BLOCK = 152;
+    var QUARTZ_ORE = 153;
+
+    var HOPPER_BLOCK = 154;
+    var QUARTZ_BLOCK = 155;
+    var QUARTZ_STAIRS = 156;
+    var DOUBLE_WOOD_SLAB = 157;
+    var DOUBLE_WOODEN_SLAB = 157;
+    var DOUBLE_WOOD_SLABS = 157;
+    var DOUBLE_WOODEN_SLABS = 157;
+    var WOOD_SLAB = 158;
+    var WOODEN_SLAB = 158;
+    var WOOD_SLABS = 158;
+    var WOODEN_SLABS = 158;
+    var STAINED_CLAY = 159;
+    var STAINED_HARDENED_CLAY = 159;
+
+    var LEAVES2 = 161;
+    var LEAVE2 = 161;
+    var WOOD2 = 162;
+    var TRUNK2 = 162;
+    var LOG2 = 162;
+    var ACACIA_WOOD_STAIRS = 163;
+    var ACACIA_WOODEN_STAIRS = 163;
+    var DARK_OAK_WOOD_STAIRS = 164;
+    var DARK_OAK_WOODEN_STAIRS = 164;
+
+    var SLIME_BLOCK = 165;
+
+    var IRON_TRAPDOOR = 167;
+    var HAY_BALE = 170;
+    var CARPET = 171;
+    var HARDENED_CLAY = 172;
+    var COAL_BLOCK = 173;
+    var PACKED_ICE = 174;
+    var DOUBLE_PLANT = 175;
+
+    var DAYLIGHT_DETECTOR_INVERTED = 178;
+
+    var FENCE_GATE_SPRUCE = 183;
+    var FENCE_GATE_BIRCH = 184;
+    var FENCE_GATE_JUNGLE = 185;
+    var FENCE_GATE_DARK_OAK = 186;
+    var FENCE_GATE_ACACIA = 187;
+
+    var SPRUCE_DOOR_BLOCK = 193;
+    var BIRCH_DOOR_BLOCK = 194;
+    var JUNGLE_DOOR_BLOCK = 195;
+    var ACACIA_DOOR_BLOCK = 196;
+    var DARK_OAK_DOOR_BLOCK = 197;
+
+    var GRASS_PATH = 198;
+    var ITEM_FRAME_BLOCK = 199;
+
+    var PODZOL = 243;
+    var BEETROOT_BLOCK = 244;
+    var STONECUTTER = 245;
+    var GLOWING_OBSIDIAN = 246;
